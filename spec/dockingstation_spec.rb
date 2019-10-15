1bbd74b246a5b3065c54afeccc12d8047088d428
Emma Van Oss <e.vanoss@gmail.com>
2019-10-15 15:51:59 +0100

    added method release_bike to DockingStation and wrote related test

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,4 +1,9 @@
 require 'dockingstation'
 
 describe DockingStation do
+
+    it "instances of DockingStation respond to the method release_bike" do
+        expect(DockingStation.new).to respond_to('release_bike')
+    end
+
 end
