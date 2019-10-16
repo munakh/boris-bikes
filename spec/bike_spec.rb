4b9836ee44ca42aad058bae3ce5ec09dac84fd4e
Student <student@Makerss-MacBook-Pro.local>
2019-10-16 14:43:06 +0100

    included gets bike and working? spec in dockingstation

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,8 +2,8 @@ require 'bike'
 
 describe Bike do
 
-    it "instances of Bike respond to the method 'working?'" do
+    it "instances of Bike respond to the method be_working" do
     expect(Bike.new).to respond_to('working?')
     end
-    
+
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -3,7 +3,16 @@ require 'dockingstation'
 describe DockingStation do
 
     it "instances of DockingStation respond to the method release_bike" do
-        expect(DockingStation.new).to respond_to('release_bike')
+      expect(subject).to respond_to :release_bike
+    end
+
+    it "releases working bikes" do
+      bike = subject.release_bike
+      expect(bike).to be_working
+    end
+
+    it "instances of DockingStation respond to the method working?" do
+      expect(subject.release_bike).to respond_to :working?
     end
 
 end
